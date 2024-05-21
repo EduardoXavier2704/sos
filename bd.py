db88f8ebf030953f8a21cce0cd88ca6ee4d9a723
Eduardo.Xavier <eduardobx1234@gmail.com>
2024-05-21 11:49:52 -0300

    Quase pronto

---
+++ bd.py
@@ -0,0 +1,117 @@
+def insert(mydb, Nome, Cpf, Data_nasc, Email, Endereco, Lugar_referencia):
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO ocorrencia (Nome, Cpf, Data_nasc, Email, Endereco, Lugar_referencia) VALUES (%s, %s, %s, %s, %s, %s)"
+    val = (Nome, Cpf, Data_nasc, Email, Endereco, Lugar_referencia)
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "Inserido com Sucesso.")
+
+    mycursor.close()
+
+def insert_proximo(mydb, Nome_completo_proximo, Cpf_proximo, Data_nasc_proximo, Email_proximo, Endereco_proximo, Local_referencia):
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO ocorrencia_proximo (Nome_completo_proximo, Cpf_proximo, Data_nasc_proximo, Email_proximo, Endereco_proximo, Local_referencia) VALUES (%s, %s, %s, %s, %s, %s)"
+    val = (Nome_completo_proximo, Cpf_proximo, Data_nasc_proximo, Email_proximo, Endereco_proximo, Local_referencia)
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "Inserido com Sucesso.")
+
+    mycursor.close()
+
+def update(mydb, titulo_antigo, titulo_novo, autor, ano, status_):
+    mycursor = mydb.cursor()
+
+    sql = "UPDATE livros SET titulo = %s, autor = %s, ano = %s, status_ = %s WHERE titulo = %s"
+    val = (titulo_novo, autor, ano, status_, titulo_antigo)
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "registro(s) atualizado(s).")
+
+    mycursor.close()
+
+
+def delete(mydb, titulo):
+    mycursor = mydb.cursor()
+
+    sql = "DELETE FROM livros WHERE titulo = %s"
+    val = (titulo,)
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "registro(s) excluído(s).")
+
+    mycursor.close()
+
+
+
+def query(mydb):
+    mycursor = mydb.cursor()
+
+    sql = "SELECT titulo, autor, ano, status_ FROM livros"
+    mycursor.execute(sql)
+
+    rows = mycursor.fetchall()
+
+    # Cria uma lista vazia para armazenar os dados
+    dados = []
+
+    # Itera sobre cada linha retornada pela consulta e adiciona os dados formatados à lista
+    for row in rows:
+        # Verifica se o valor é None antes de formatá-lo na string de impressão
+        titulo = row[0] if row[0] is not None else ""
+        autor = row[1] if row[1] is not None else ""
+        ano = row[2] if row[2] is not None else ""
+        status_ = "Disponível" if row[3] else "Indisponível" if row[3] is not None else ""
+
+        dados.append("{: <20} {: <30} {: <10} {: <10}".format(
+            titulo, autor, ano, status_))
+
+    # Retorna a lista de dados
+    return dados
+
+def register(mydb, nome, email, celular, senha):
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO registro_usuario (nome, email, celular, senha) VALUES (%s, %s, %s, %s)"
+    val = (nome, email, celular, senha)
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print("Usuário registrado com sucesso.")
+
+    mycursor.close()
+
+
+def login(mydb, nome, senha):
+    mycursor = mydb.cursor()
+
+    sql = "SELECT * FROM usuarios WHERE nome = %s AND senha = %s"
+    val = (nome, senha)
+
+    mycursor.execute(sql, val)
+
+    user = mycursor.fetchone()
+
+    if user:
+        print("Login bem-sucedido.")
+        return user
+    else:
+        print("Credenciais inválidas.")
+        return None
+
+
--- login_cadastro.py
+++ login_cadastro.py
@@ -1,4 +1,8 @@
 from kivy.app import App
+from conexao import connect
+from bd import insert, update, delete, query, register, login
+from datetime import datetime
+from usuario import Usuario
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
@@ -6,6 +10,8 @@ from functools import partial
 from kivy.uix.modalview import ModalView
 from kivy.lang import Builder
 import os
+
+mydb = connect()
 
 # Obter o caminho absoluto do diretório onde o script está localizado
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -58,14 +64,23 @@ class Login(FloatLayout):
         self.senha_input = self.ids.senha_input
 
         self.cadastrar_button = self.ids.cadastrar_button
-        self.cadastrar_button.bind(on_release=partial(self.create_new_telainicial))
+        self.cadastrar_button.bind(on_release=partial(self.check_login))
         self.login_button = self.ids.login_button
         self.login_button.bind(on_release=partial(self.create_new_window))
 
-    def create_new_telainicial(self, instance):
-        telainicial = TelaInicial()
-        telainicial.open()
-        Window.clearcolor = (0, 0, 0, 1)
+    def check_login(self, instance):
+        nome = self.username_input.get()
+        senha = self.senha_input.get()
+
+        user = login(mydb, nome, senha)
+
+        if user:
+            print("Login bem-sucedido!")
+            telainicial = TelaInicial()
+            telainicial.open()
+        else:
+            print("Credenciais inválidas!")
+            return None
 
     def create_new_window(self, instance):
         new_window = NewWindow()
@@ -91,7 +106,7 @@ class NewWindow(FloatLayout):
         self.senha_input = self.ids.senha_input
 
         self.button_cadastrar = self.ids.button_cadastrar
-        self.button_cadastrar.bind(on_release=partial(self.entrar_interface_login))
+        self.button_cadastrar.bind(on_release=partial(self.register_user))
         self.botao_login = self.ids.botao_login
         self.botao_login.bind(on_release=partial(self.botao_voltar_login))
 
@@ -99,10 +114,14 @@ class NewWindow(FloatLayout):
         voltar_login = Login()
         voltar_login.open()
 
-    def entrar_interface_login(self, instance):
-        entrar_login = Login()
-        entrar_login.open()
-        Window.clearcolor = (0, 0, 0, 1)  # Mudar a cor de fundo para preto
+    def register_user(self, instance):
+        user_data = {
+            'nome': self.username_input.get(),
+            'email': self.email_input.get(),
+            'celular': self.celular_input.get(),
+            'senha': self.senha_input.get()
+        }
+        register(mydb, user_data['nome'], user_data['email'], user_data['celular'], user_data['senha'])
 
     def open(self):
         self._window = ModalView(size_hint=(1, 1))
@@ -156,8 +175,19 @@ class ListaOcorrencias(FloatLayout):
     def on_kv_post(self, base_widget):
         # Widgets são acessíveis após o arquivo KV ser carregado
         self.lista_ocorrencia = self.ids.lista_ocorrencia
+        self.lista_ocorrencia.bind(on_release=partial(self.abrir_lista_assalto))
+        self.lista_assedio = self.ids.lista_assedio
+        self.lista_assedio.bind(on_release=partial(self.abrir_lista_assedio))
         self.voltar_telainicial = self.ids.voltar_telainicial
         self.voltar_telainicial.bind(on_release=partial(self.voltar_para_telainicial))
+
+    def abrir_lista_assedio(self, instance):
+        lista_assedio = ListaDenunciaAssedio()
+        lista_assedio.open()
+
+    def abrir_lista_assalto(self, instance):
+        lista_assaltos = ListaDenunciaAssalto()
+        lista_assaltos.open()
 
     def voltar_para_telainicial(self, instance):
         telainicial_voltar = TelaInicial()
@@ -187,6 +217,8 @@ class AbrirOcorrencia(FloatLayout):
         self.voltarparatelainicial.bind(on_release=partial(self.voltar_para_tela_inicial))
         self.ocorrencia_prox = self.ids.ocorrencia_prox
         self.botao_enviar_ocorrencia = self.ids.botao_enviar_ocorrencia
+        self.endereço_amigo = self.ids.endereco_amigo
+        self.reference = self.ids.reference
 
     def voltar_para_tela_inicial(self, instance):
         icone_casa = TelaInicial()
@@ -211,10 +243,63 @@ class Ocorrencia(FloatLayout):
         self.enviar_ocorrencia_botao = self.ids.enviar_ocorrencia_botao
         self.seta_voltar = self.ids.seta_voltar
         self.seta_voltar.bind(on_release=partial(self.apertar_voltar))
+        self.nomeseu_completo = self.ids.nomeseu_completo
+        self.seucpf = self.ids.seucpf
+        self.suadata_nasc = self.ids.suadata_nasc
+        self.seuemail = self.ids.seuemail
+        self.seuendereco = self.ids.seuendereco
+        self.suareferencia = self.ids.suareferencia
+        self.suadescricao = self.ids.suadescricao
 
     def apertar_voltar(self, instance):
         voltar_casinha = TelaInicial()
         voltar_casinha.open()
+
+    def open(self):
+        self._window = ModalView(size_hint=(1, 1))
+        self._window.add_widget(self)
+        self._window.background_color = (0, 0, 0, 1)  # Definir o fundo do ModalView como preto
+        self._window.open()
+
+class ListaDenunciaAssalto(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Window.clearcolor = (1, 1, 1, 1)
+        self.arg1 = None  # Inicializando arg1 como None
+        self.arg2 = None  # Inicializando arg2 como None
+
+    def on_kv_post(self, base_widget):
+        # Widgets são acessíveis após o arquivo KV ser carregado
+        self.listar_assalto = self.ids.listar_assalto
+        self.setinha = self.ids.setinha
+        self.setinha.bind(on_release=partial(self.setinha_voltar))
+
+    def setinha_voltar(self, instance):
+        setinha = ListaOcorrencias()
+        setinha.open()
+
+    def open(self):
+        self._window = ModalView(size_hint=(1, 1))
+        self._window.add_widget(self)
+        self._window.background_color = (0, 0, 0, 1)  # Definir o fundo do ModalView como preto
+        self._window.open()
+
+class ListaDenunciaAssedio(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Window.clearcolor = (1, 1, 1, 1)
+        self.arg1 = None  # Inicializando arg1 como None
+        self.arg2 = None  # Inicializando arg2 como None
+
+    def on_kv_post(self, base_widget):
+        # Widgets são acessíveis após o arquivo KV ser carregado
+        self.listar_assedio = self.ids.listar_assedio
+        self.miniseta = self.ids.miniseta
+        self.miniseta.bind(on_release=partial(self.miniseta_voltar))
+
+    def miniseta_voltar(self, instance):
+        volte = ListaOcorrencias()
+        volte.open()
 
     def open(self):
         self._window = ModalView(size_hint=(1, 1))
@@ -228,5 +313,9 @@ class MyApp(App):
         
 if __name__ == '__main__':
     MyApp().run()
+
+
+mydb.close()
+
 
 
